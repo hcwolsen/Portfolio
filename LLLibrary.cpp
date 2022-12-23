14f11799baab9b33798dd4e0acd99e61bd657768
hcwolsen <97662511+hcwolsen@users.noreply.github.com>
2022-12-22 23:47:16 -0800

    Changes nearly finalized

--- LLLibrary.cpp
+++ LLLibrary.cpp
@@ -1,34 +1,151 @@
+//This program file was used for debugging while I was writing LinkedList.h, 
+//and is provided to show exhaustive testing of each member function for 
+//objects storing several primitive data types, and one abstract type
 #include <iostream>
 #include "LinkedList.h"
 using namespace std;
 
 int main() {
 
+	cout << "Creating a LinkedList object of type string and appending five values: ";
 	LinkedList<string> stringList;
+	stringList.append("Zero");
+	stringList.append("One");
+	stringList.append("Two");
+	stringList.append("Three");
+	stringList.append("Four");
+	cout << stringList.getPrintable() << endl;
+	cout << "Success\n";
+
+	cout << "\nCalling the copy constructor on the original object and printing the copy's contents: ";
 	LinkedList<string> stringList2(stringList);
-	stringList.append("Hello!");
-	stringList.append("Hello again!");
-	stringList.contains("Hello!");
-	stringList.contains("Hola!");
+	cout << stringList2.getPrintable() << endl;
+	cout << "Success\n";
+
+	cout << "\nCalling the reverse() function on the original list to verify the copy is a shallow copy...";
+	stringList.reverse();
+	cout << "\nOriginal list's new contents: ";
+	cout << stringList.getPrintable() << endl;
+	cout << "Copy's contents: ";
+	cout << stringList2.getPrintable() << endl;
+	cout << "Copy's contents are unaffected, success\n";
+
+	cout << "\nCalling the remove() function on the original object and removing the last two nodes...\n";
+	stringList.remove("Three");
+	stringList.remove("Four");
+	cout << "Object's new contents: ";
+	cout << stringList.getPrintable() << endl;
+	cout << "Success\n";
+
+	cout << "\nTesting getHead() and getTail() on original object...\n";
+	cout << "getHead(): " << stringList.getHead() << endl;
+	cout << "getTail(): " << stringList.getTail() << endl;
+	cout << "Success\n";
+
+	cout << "\nTesting contains() on the original object...\n";
+	cout << "Original object's contents: ";
+	cout << stringList.getPrintable() << endl;
+	cout << "Result of contains(\"One\"): " << stringList.contains("One") << endl;
+	cout << "Success\n";
+
+	cout << "\nTesting getSize() by calling it on both the original and copy...";
+	cout << "\nSize of the original: " << stringList.getSize() << endl;
+	cout << "Size of the copy: " << stringList2.getSize() << endl;
 
 
 
-	/*
-	int llSize;
-	string val;
+	cout << "Creating a LinkedList object of type int and appending five values: ";
+	LinkedList<int> intList;
+	intList.append(0);
+	intList.append(1);
+	intList.append(2);
+	intList.append(3);
+	intList.append(4);
+	cout << intList.getPrintable() << endl;
+	cout << "Success\n";
 
-	cout << "Enter linked list size: ";
-	cin >> llSize;
-	LinkedList<string> ll;
+	cout << "\nCalling the copy constructor on the original object and printing the copy's contents: ";
+	LinkedList<int> intList2(intList);
+	cout << intList2.getPrintable() << endl;
+	cout << "Success\n";
 
-	for (int x = 0; x < llSize; x++) {
-		cout << "Enter a new value: ";
-		cin >> val;
-		ll.append(val);
-	}
+	cout << "\nCalling the reverse() function on the original list to verify the copy is a shallow copy...";
+	intList.reverse();
+	cout << "\nOriginal list's new contents: ";
+	cout << intList.getPrintable() << endl;
+	cout << "Copy's contents: ";
+	cout << intList2.getPrintable() << endl;
+	cout << "Copy's contents are unaffected, success\n";
 
-	cout << ll.print();
-	ll.remove(val);
-	cout << ll.print();
-	*/
+	cout << "\nCalling the remove() function on the original object and removing the last two nodes...\n";
+	intList.remove(3);
+	intList.remove(4);
+	cout << "Object's new contents: ";
+	cout << intList.getPrintable() << endl;
+	cout << "Success\n";
+
+	cout << "\nTesting getHead() and getTail() on original object...\n";
+	cout << "getHead(): " << intList.getHead() << endl;
+	cout << "getTail(): " << intList.getTail() << endl;
+	cout << "Success\n";
+
+	cout << "\nTesting contains() on the original object...\n";
+	cout << "Original object's contents: ";
+	cout << intList.getPrintable() << endl;
+	cout << "Result of contains(\"One\"): " << intList.contains(1) << endl;
+	cout << "Success\n";
+
+	cout << "\nTesting getSize() by calling it on both the original and copy...";
+	cout << "\nSize of the original: " << intList.getSize() << endl;
+	cout << "Size of the copy: " << intList2.getSize() << endl;
+
+
+
+	cout << "Creating a LinkedList object of type int and appending five values: ";
+	LinkedList<char> charList;
+	charList.append('a');
+	charList.append('b');
+	charList.append('c');
+	charList.append('d');
+	charList.append('e');
+	cout << charList.getPrintable() << endl;
+	cout << "Success\n";
+
+	cout << "\nCalling the copy constructor on the original object and printing the copy's contents: ";
+	LinkedList<char> charList2(charList);
+	cout << charList2.getPrintable() << endl;
+	cout << "Success\n";
+
+	cout << "\nCalling the reverse() function on the original list to verify the copy is a shallow copy...";
+	charList.reverse();
+	cout << "\nOriginal list's new contents: ";
+	cout << charList.getPrintable() << endl;
+	cout << "Copy's contents: ";
+	cout << charList2.getPrintable() << endl;
+	cout << "Copy's contents are unaffected, success\n";
+
+	cout << "\nCalling the remove() function on the original object and removing the last two nodes...\n";
+	charList.remove('d');
+	charList.remove('e');
+	cout << "Object's new contents: ";
+	cout << charList.getPrintable() << endl;
+	cout << "Success\n";
+
+	cout << "\nTesting getHead() and getTail() on original object...\n";
+	cout << "getHead(): " << charList.getHead() << endl;
+	cout << "getTail(): " << charList.getTail() << endl;
+	cout << "Success\n";
+
+	cout << "\nTesting contains() on the original object...\n";
+	cout << "Original object's contents: ";
+	cout << charList.getPrintable() << endl;
+	cout << "Result of contains(\"One\"): " << charList.contains('a') << endl;
+	cout << "Success\n";
+
+	cout << "\nTesting getSize() by calling it on both the original and copy...";
+	cout << "\nSize of the original: " << charList.getSize() << endl;
+	cout << "Size of the copy: " << charList2.getSize() << endl;
+	
+
+	cout << "Functions reverse() and getPrintable() were not tested independently since they were used in the process of testing other functions.\n\n";
 }
--- LinkedList.h
+++ LinkedList.h
@@ -1,3 +1,7 @@
+//A primitive linked list class designed to work well with any primitive type
+//Credit to my friend Kai Tamkun (Github @ heimskr) for helping to refresh my memory on some of the intricacies of pointer usage
+//LLLibrary.cpp is provided on my Github (@ hcwolsen) to test and demonstrate each member function
+
 #ifndef _LinkedList_h_
 #define _LinkedList_h_
 
@@ -8,62 +12,55 @@ public:
 	LinkedList(const LinkedList<T>& original);
 	void append(const T& value);
 	void remove(const T& value);
+	T getHead();
+	T getTail();
 	bool contains(const T& value);
-	/*
 	void reverse();
-	T operator[](const int index) const;
-	T& operator[](const int index);
 	int getSize();
-	std::string print();
-	*/
-	private:
-		struct Node {
-			T value;
-			Node* next;
-		};
-		Node head;
-		int size = 0;
+	std::string getPrintable();
+
+private:
+	struct Node {
+		T value = T();
+		Node* next = nullptr;
+	};
+	Node* head = new Node{ T(), nullptr };
+	int size = 0;
 };
 
 template <typename T>
 LinkedList<T>::LinkedList() {
-	head.value = T();
-	head.next = nullptr;
+	head->value = T();
+	head->next = nullptr;
 }
 
 template <typename T>
 LinkedList<T>::LinkedList(const LinkedList<T>& original) {
 
-	head.value = original.head.value;
-	head.next = original.head.next;
-	Node *cur = head.next;
-	Node *ogCur = original.head.next;
+	if (original.size == 0 || original.head == nullptr) {
+		return;
+	}
 
+	this->size = original.size;
 
-	for (int x = 0; x <= original.size; x++) {
+	Node* newHead = this->head;
+	Node* originalHead = original.head;
 
-		if (ogCur == nullptr) {
-			break;
-		}
-		else {
-			cur->value = ogCur->value;
-			cur->next = ogCur->next;
-			if (ogCur->next = nullptr) {
-				break;
-			}
-		}
-		
-		size++;
+	for (int x = 0; x < original.size; x++) {
+		newHead->value = originalHead->value;
+		newHead->next = new Node{ T(), nullptr };
+		originalHead = originalHead->next;
+		newHead = newHead->next;
 	}
 }
 
 template <typename T>
 void LinkedList<T>::append(const T& value) {
-	Node *h = &head;
-	Node *a = ( value, nullptr );
-	
+	Node* h = head;
+	Node* a = new Node{ value, nullptr };
+
 	if (size == 0) {
-		head.value = value;
+		head->value = value;
 		size++;
 	}
 	else {
@@ -77,7 +74,7 @@ void LinkedList<T>::append(const T& value) {
 
 template <typename T>
 bool LinkedList<T>::contains(const T& value) {
-	Node* h = &head;
+	Node* h = head;
 
 	if (size == 0) {
 		return false;
@@ -96,45 +93,72 @@ bool LinkedList<T>::contains(const T& value) {
 	}
 }
 
-
+//Removes all instances of a specified value from the linked list
 template <typename T>
 void LinkedList<T>::remove(const T& value) {
-	Node* h = &head;
+	Node* h = head;
 	Node* prev = h;
-
-	if (size == 1 && head.value == value) {
-		delete head;
-	}
 
 	if (!contains(value)) {
 		return;
 	}
+	else if (size == 1 && head->value == value) {
+		head->value = T();
+		size = 0;
+		return;
+	}
 	else {
-		while (h.next != nullptr) {
+		while (h->next != nullptr) {
 			if (h->value == value) {
-				
+				h = h->next;
+				prev->next = h;
 				size--;
+			}
+			else {
+				h = h->next;
+				prev = prev->next;
 			}
 		}
 	}
 }
 
-/*
 template <typename T>
 void LinkedList<T>::reverse() {
+	Node* prev = head;
+	Node* cur = head->next;
+	Node* tempPrev = new Node{ T(), nullptr };
+	Node* tempCur = new Node{ T(), nullptr };
 
+	if (size == 0 || size == 1) {
+		return;
+	}
+
+	prev->next = nullptr;
+
+	while (cur->next != nullptr) {
+		tempPrev = cur;
+		tempCur = cur->next;
+
+		cur->next = prev;
+
+		cur = tempCur;
+		prev = tempPrev;
+	}
+
+	cur->next = prev;
+	head = cur;
 }
- 
+
+
 template <typename T>
 int LinkedList<T>::getSize() {
 	return size;
 }
 
 template <typename T>
-std::string LinkedList<T>::print() {
+std::string LinkedList<T>::getPrintable() {
 	std::string printableList;
-	Node *h = &head;
-
+	Node* h = head;
 	while (h != nullptr) {
 		printableList.append(h->value);
 		if (h->next != nullptr) {
@@ -142,52 +166,38 @@ std::string LinkedList<T>::print() {
 		}
 		h = h->next;
 	}
-	
+
 	return printableList;
 }
 
 template <typename T>
-T LinkedList<T>::operator[](const int index) const {
-	//const& T indexVal = T();
-	Node* h = &head;
-	int count = 0;
+T LinkedList<T>::getHead() {
 
-	if (index >= size) {
+	if (size == 0) {
 		return T();
 	}
 
-	while (h != nullptr) {
-
-		if (count == index) {
-			return h->value;
-		}
-
-		h = h->next;
-
-		count++;
-	}
+	return head->value;
 }
 
 template <typename T>
-T& LinkedList<T>::operator[](const int index) {
-	//const& T indexVal = T();
-	Node* h = &head;
-	int count = 0;
+T LinkedList<T>::getTail() {
+	Node* h = head;
 
-	if (index >= size) {
+	if (size == 0) {
 		return T();
 	}
 
-	while (h != nullptr) {
-
-		if (count == index) {
-			return h->value;
-		}
-
-		h = h->next;
-
-		count++;
+	if (size == 1) {
+		return head->value;
 	}
+
+	while (h->next != nullptr) {
+		h = h->next;
+	}
+
+	return h->value;
+
 }
-*/
+
 #endif
